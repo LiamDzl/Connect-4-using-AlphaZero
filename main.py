d21aab6db50821ef20b863b6f8bc62f3df56584b
Liam Dalziel <liamdzl100@gmail.com>
2025-11-18 18:14:53 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import torch
 from connect_4 import Grid, winner, graphic
 import time
 
-# policy_network = model(structure=[42,100,100,42,8,7], # 7 scalars for distibution, and the last scalar in the penultimate vector encodes value function
-#            activation="softmax")
+policy_network = policy(structure=[42,100,100,42,8])
 
 x = torch.zeros(6,7)
 environment = Grid(state=x)
@@ -17,34 +16,74 @@ print("(Enter \"end\" to exit)\n")
 graphic(environment.state)
 print("")
 
+move = 0
+
 while (column != "end"):
-    column = input("🔴 Select any column from 1 to 7: ")
+
+    proceed = False
+    inrange = False
+    
+    while not proceed:
+        column = input("🔴 Select any column from 1 to 7: ")
+        print(f"\n----------------------- ( Move {move} )")
+        colNum = -1
+        if column == "end":
+            
+            break
+        try:
+            colNum = int(column)
+        except:
+            print("\n❌ ERROR: not in range\n")
+
+        if colNum >= 0:
+            if colNum >= 1 and colNum <= 7:
+                proceed = True
+                inrange = True
+
+            if inrange == True:
+                if environment.state[0, int(column) - 1] != 0:
+                    print("\n❌ ERROR: column full!\n")
+                    proceed = False
+            
+            else:
+                print("\n❌ ERROR: not in range\n") 
+            
 
     if column == "end":
         break
 
     else:
-        print("\n")
         environment.action(player=1, column=int(column)-1)
+        move += 1
 
         if winner(environment.state) == 1:
             print("\n🔴🔴🔴🔴 Red Wins! 🔴🔴🔴🔴\n")
-            print("\n")
+            graphic(environment.state)
             column = "end"
-        
-        graphic(environment.state)
-        
+
+        print("\n")
+
         if column == "end":
             break
 
+        else:
+            graphic(environment.state)
+        
+        value, distribution = policy_network.forward(environment.state)
+        distribution = distribution.detach()
         time.sleep(0.8)
-        yellow = np.random.randint(7)
+
+        yellow_sample = torch.multinomial(distribution, num_samples=1)
+
         print("\n")
-        environment.action(player=-1, column=int(yellow))
+        environment.action(player=-1, column=int(yellow_sample.item()))
+        print("Value Function: ", value.item())
+        print("Distribution: ", distribution)
+        print("\n")
         graphic(environment.state)
         print("\n")
 
         if winner(environment.state) == -1:
-            print("\n🟡🟡🟡🟡 Yellow Wins! 🟡🟡🟡🟡\n")
+            print("🟡🟡🟡🟡 Yellow Wins! 🟡🟡🟡🟡\n")
             column = "end"
 
--- neural_network.py
+++ neural_network.py
@@ -2,16 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 from connect_4 import mask
 
 class policy(nn.Module):
     def __init__(self, structure): 
-        super().__init__() # because of inheritance
+        super().__init__()
         layers = [] # this object is a python list, but contains actual affine maps 
 
         for i in range(len(structure) - 1):  # n layers need n-1 Linear maps
@@ -31,17 +28,15 @@ class policy(nn.Module):
      
     def forward(self, state): # mask a list of columns that are full, i.e. top element non-zero - btw mask comes automatically from state
         x = state.reshape(1,42)
-        for i in self.layers[:-1]: # ReLU, up til penultimate (since we wanna grab this vector)
+        for i in self.layers[:-1]: # ReLU, up til last (since we wanna grab this vector)
             x = F.relu(i(x))
 
         penultimate_eight = self.layers[-1](x)
         value = penultimate_eight[:,-1]
         filter = mask(state)
         to_softmax = penultimate_eight[filter] #<=7
-        print(to_softmax)
 
         reduced_distribution = F.softmax(to_softmax, dim = 0)
-        print(reduced_distribution)
         distribution = torch.zeros(7,1)
         count = 0
         for i in range(7):
@@ -49,4 +44,4 @@ class policy(nn.Module):
                 distribution[i] = reduced_distribution[count]
                 count += 1
         
-        return value, distribution
+        return value, distribution.T
