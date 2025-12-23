afdfa83af97035dbaf6b3c38a1c8ecb5feb529ae
Liam Dalziel <liamdzl100@gmail.com>
2025-12-23 23:09:01 +0000

    Update selfplay.py

--- selfplay.py
+++ selfplay.py
@@ -2,20 +2,21 @@ import torch
 from neural_network import policy
 from selfplay_functions import parallel_selfplay
 from connect_4 import graphic
+import copy
 
 # Import Model, [42, 64, 64, 64, 8]
 torch.serialization.add_safe_globals([policy])
-policy_network = torch.load("1000itr lr=0.1 expc=1.85.pt", weights_only=False)
+policy_network = torch.load("1000itr lr=0.001 expc=1.85.pt", weights_only=False)
 
 cpu_cores = 8
 game_sets = 125
 
 # Hyperparameters
-exploration_constant = 1.85
+exploration_constant = 1.35
 loss_value_constant = 1
 loss_dist_constant = 1
 epochs = 3
-nabla = 0.1
+nabla = 0.001
 noise = 0
 
 if __name__ == "__main__":
@@ -26,17 +27,38 @@ if __name__ == "__main__":
                                  cpu_cores=cpu_cores,
                                  noise=noise)
 
-        for index, state in enumerate(X):
-            print(state.reshape(6,7))
+        X_original = X.clone()
+        Y_original = Y.clone()
+
+        # Augment Data - Use Symmetry of Connect 4
+        X_mirror = X.clone()
+        Y_mirror = Y.clone()
+
+        for index, state in enumerate(X_original):
+            shaped = state.reshape(6,7)
+            print(shaped)
+            shaped_mirror = torch.flip(shaped, dims=[1])
+            X_mirror[index] = shaped_mirror.reshape(42)
             print("")
-            print(Y[index])
+            print(shaped_mirror)
+            print("")
+            y = Y_original[index]
+            y_dist = y[:7]
+            y_value = y[7]
+            y_mirror = torch.cat((torch.flip(y_dist, dims=[0]), torch.tensor([y_value])), dim=0)
+            Y_mirror[index] = y_mirror
+            print(y)
+            print(y_mirror)
             print("")
 
-        policy_network.train(training_inputs=X,
-                             training_outputs=Y,
+        X_symm = torch.cat((X_original, X_mirror), dim=0)
+        Y_symm = torch.cat((Y_original, Y_mirror), dim=0)
+
+        policy_network.train(training_inputs=X_symm,
+                             training_outputs=Y_symm,
                              loss_value_constant=loss_value_constant,
                              loss_dist_constant=loss_dist_constant,
                              epochs=epochs,
                              nabla=nabla)
 
-        torch.save(policy_network, "2000itr lr=0.1 expc=1.85.pt")
+        torch.save(policy_network, "1000itr lr=0.001 expc=1.85.pt")
