b205a5eb43de4ab0b4ff5d7d8c9c01b93b990962
Liam Dalziel <liamdzl100@gmail.com>
2025-12-23 21:55:46 +0000

    Update connect_4.py

--- connect_4.py
+++ connect_4.py
@@ -2,6 +2,11 @@ import numpy as np
 import torch
 import copy
 
+def compute_player(state):
+    if torch.sum(state) == 0:
+        return 1
+    else:
+        return -1
 
 def mask(state):
     board = state.reshape(6, 7)
@@ -12,41 +17,44 @@ def mask(state):
 
 
 class Grid():
-    def __init__(self, state, player): # State a 6 x 7 tensor, player either +/- 1
+    def __init__(self, state): # State a 6 x 7 tensor, encodes player
         self.state = copy.deepcopy(state)
-        self.player = player
+        self.player = compute_player(self.state)
 
     def action(self, column):
         mutable_state = self.state
 
         if mutable_state[0, column] != 0:
-            return "### Illegal Action ###"
+            return "# Illegal Action"
         
         else:
             for i in range(6):
                 check = mutable_state[5-i, column]
                 if int(check.item()) == 0:
-                    mutable_state[5-i, column] = self.player
-                    self.player = self.player * -1 # Change player 
+                    mutable_state[5-i, column] = 1 # Always +1 Perspective
                     break
 
-            return "### Legal Action ###"
+            # Finally, Flip
+            mutable_state *= -1
+            self.player = compute_player(mutable_state)
+            
+            return "# Legal Action"
         
 
-def winner(state): # 6+6 diags, 6 rows and 7 columns to check - id first make them vectors in own right (easy), givem names like TL_BR_4 , and from there check if four in a row
+def winner(state):
     # Check rows   
     for i in range(6):
         row = state[i]
         for j in range(row.size(0) - 3):
             if row[j] == row[j+1] and row[j+1] == row[j+2] and row[j+2] == row[j+3] and int(row[j].item()) != 0:
-                return int(row[j].item())
+                return 1
 
     # Check columns   
     for i in range(7):
         column = state[:, i]
         for j in range(column.size(0) - 3):
             if column[j] == column[j+1] and column[j+1] == column[j+2] and column[j+2] == column[j+3] and int(column[j].item()) != 0:
-                return int(column[j].item())
+                return 1
          
     # Check " \ " diagonals
 
@@ -54,7 +62,7 @@ def winner(state): # 6+6 diags, 6 rows and 7 columns to check - id first make th
         diagonal = torch.diagonal(state, offset=i-2, dim1=0, dim2=1)
         for j in range(diagonal.size(0) - 3):
             if diagonal[j] == diagonal[j+1] and diagonal[j+1] == diagonal[j+2] and diagonal[j+2] == diagonal[j+3] and int(diagonal[j].item()) != 0:
-                return int(diagonal[j].item())
+                return 1
     
     # Check " / " diagonals
 
@@ -64,18 +72,31 @@ def winner(state): # 6+6 diags, 6 rows and 7 columns to check - id first make th
         diagonal = torch.diagonal(mirror_state, offset=i-2, dim1=0, dim2=1)
         for j in range(diagonal.size(0) - 3):
             if diagonal[j] == diagonal[j+1] and diagonal[j+1] == diagonal[j+2] and diagonal[j+2] == diagonal[j+3] and int(diagonal[j].item()) != 0:
-                return int(diagonal[j].item())
+                return 1
             
     return 0
 
 def graphic(state):
-    for row in state:
-        for j in row:
-            if int(j.item()) == 1:
-                print("🔴", end=" ")
-            elif int(j.item()) == -1:
-                print("🟡", end=" ")
-            elif int(j.item()) == 0:
-                print("⚫", end=" ")
+    # Red to Move
+    if torch.sum(state) == 0:
+        for row in state:
+            for j in row:
+                if int(j.item()) == 1:
+                    print("🔴", end=" ")
+                elif int(j.item()) == -1:
+                    print("🟡", end=" ")
+                elif int(j.item()) == 0:
+                    print("⚫", end=" ")
+            print("")
 
-        print("")
+    # Yellow to Move
+    else:
+        for row in state:
+            for j in row:
+                if int(j.item()) == 1:
+                    print("🟡", end=" ")
+                elif int(j.item()) == -1:
+                    print("🔴", end=" ")
+                elif int(j.item()) == 0:
+                    print("⚫", end=" ")
+            print("")
