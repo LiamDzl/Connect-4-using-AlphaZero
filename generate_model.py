3cff658266fa246a4cd8d11cc29fc9f2786899a0
Liam Dalziel <liamdzl100@gmail.com>
2025-12-30 15:41:28 +0000

    Update generate_model.py

--- generate_model.py
+++ generate_model.py
@@ -1,5 +1,8 @@
 import torch
 from neural_network import policy
 
-parameters = policy(structure=[42, 64, 64, 64, 8])
+parameters = policy(trunk_structure=[84, 168, 168, 168],
+                    policy_structure=[168, 84, 7],
+                    value_structure=[168, 84, 1])
+
 torch.save(parameters, "random_parameters.pt")
