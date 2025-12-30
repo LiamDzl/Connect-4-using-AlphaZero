5274240a85ab934a7e6fd77f07d1bfee60ba7deb
Liam Dalziel <liamdzl100@gmail.com>
2025-12-30 15:36:56 +0000

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -4,94 +4,127 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 from tree_module import MCTS
-from functions import alphazero_display
-from connect_4 import mask, graphic
+from functions import alphazero_display, expand_to_84
+from connect_4 import mask, graphic, compute_player
 
 class policy(nn.Module):
-    def __init__(self, structure):
+    def __init__(self, trunk_structure, policy_structure, value_structure):
         super().__init__()
-        layers = [] # this object is a python list, but contains actual affine maps 
 
-        for i in range(len(structure) - 1):  # n layers need n-1 Linear maps
-            layers.append(nn.Linear(structure[i], structure[i + 1]))
+        trunk_layers = []
+        policy_layers = []
+        value_layers = []
 
-        self.structure = structure
-        self.layers = nn.ModuleList(layers)
-        self.pspace_dimensions = []
+        # Main Trunk
+        for i in range(len(trunk_structure) - 1):
+            trunk_layers.append(nn.Linear(trunk_structure[i], trunk_structure[i + 1]))
 
-        for i in range(len(structure) - 1):
-            self.pspace_dimensions.append(structure[i] * structure[i+1]) # size of weight matrix
-            self.pspace_dimensions.append(structure[i+1]) # size of bias vector
+        self.trunk_structure = trunk_structure
+        self.trunk_layers = nn.ModuleList(trunk_layers)
 
-        self.total_dimension = 0
-        for i in self.pspace_dimensions:
-            self.total_dimension += i
+        # Policy Arm
+        for i in range(len(policy_structure) - 1):
+            policy_layers.append(nn.Linear(policy_structure[i], policy_structure[i + 1]))
+
+        self.policy_structure = policy_structure
+        self.policy_layers = nn.ModuleList(policy_layers)
+
+        # Value Arm
+        for i in range(len(value_structure) - 1):
+            value_layers.append(nn.Linear(value_structure[i], value_structure[i + 1]))
+
+        self.value_structure = value_structure
+        self.value_layers = nn.ModuleList(value_layers)
+
 
     def forward(self, x):
-        state = x.reshape(6,7)
+        # Save for Masking
+        state_me = x[:42].reshape(6,7) 
+        state_them = x[42:].reshape(6,7)
 
-        for i in self.layers[:-1]: # ReLU, up til last (since we wanna grab this vector)
-            x = F.relu(i(x))
+        # Forward Main
+        for i in self.trunk_layers:
+            x = F.leaky_relu(i(x)) 
 
-        penultimate_eight = self.layers[-1](x)
-        dist = penultimate_eight[0:7]
-        value = penultimate_eight[-1]
+        x_policy = x.clone()
+        x_value = x.clone()
 
-        # Force valuation between -1 and 1
-        value = 2 * torch.sigmoid(value) - 1
+        # Forward Policy
+        for i in self.policy_layers[:-1]:
+            x_policy = F.leaky_relu(i(x_policy)) 
 
-        # Splitting value from distribution + masking
-        filter = mask(state)
-        to_softmax = dist[filter] #<=7
+        x_policy = self.policy_layers[-1](x_policy)
 
-        reduced_distribution = F.softmax(to_softmax, dim = 0)
-        
+        # Forward Value
+        for i in self.value_layers[:-1]:
+            x_value = F.leaky_relu(i(x_value))
+
+        x_value = self.value_layers[-1](x_value)
+        x_value = 2 * torch.sigmoid(x_value) - 1 # Force Value in [-1, 1]
+
+        # Mask Full Columns
+        filter_me = mask(state_me)
+        filter_them = mask(state_them)
+        filter = (filter_me & filter_them)
+
+        to_softmax = x_policy[filter]
+        reduced_distribution = F.softmax(to_softmax, dim=0)
         distribution = torch.zeros(7)
         distribution[filter] = reduced_distribution
-        
-        output_vector = torch.cat([distribution, value.unsqueeze(0)], dim=0)
-        output_vector = output_vector.reshape(8)
+        output = torch.cat([distribution, x_value.unsqueeze(0)[0]], dim=0)
+        output = output.reshape(8)
 
-        return output_vector
+        return output
     
-    def train(self, training_inputs, training_outputs, loss_value_constant, loss_dist_constant, epochs, nabla):
-            training_size = training_inputs.shape[0]
-            mse = nn.MSELoss()
-            optimiser = optim.SGD(self.parameters(), lr=nabla)
+    def train(self, training_inputs, training_outputs, epochs, nabla, batch_size=40):
+        training_size = training_inputs.shape[0]
+        mse = nn.MSELoss()
+        optimiser = optim.SGD(self.parameters(), lr=nabla)
 
-            for epoch in range(epochs):
-                permutation = torch.randperm(training_size)
-                training_inputs = training_inputs[permutation]
-                training_outputs = training_outputs[permutation]
+        for epoch in range(epochs):
+            permutation = torch.randperm(training_size)
+            training_inputs = training_inputs[permutation]
+            training_outputs = training_outputs[permutation]
 
-                for x, y in zip(training_inputs, training_outputs):
+            for i in range(0, training_size, batch_size):
+                batch_inputs = training_inputs[i: i + batch_size]
+                batch_outputs = training_outputs[i: i + batch_size]
+
+                optimiser.zero_grad()
+                loss_scalar = 0
+
+                for x, y in zip(batch_inputs, batch_outputs):
                     nn_output = self.forward(x)
 
-                    # Value and policy
                     value_loss = mse(nn_output[7], y[7])
                     policy_loss = -torch.dot(y[0:7], torch.log(nn_output[0:7] + 1e-8))
 
-                    loss_scalar = (loss_value_constant) * value_loss + (loss_dist_constant) * policy_loss
+                    loss_scalar += value_loss + policy_loss
 
-                    optimiser.zero_grad()
-                    loss_scalar.backward()
-                    optimiser.step()
+                loss_scalar.backward()
+                optimiser.step()
 
-            return "Successfully Trained."
+        return "Successfully Trained."
     
     def evaluate(self, name, state, exploration_constant, noise):
         print("\n")
         graphic(state)
         print("\n")
         state = state.reshape(42)
-        state = state.float()
-        output = self.forward(state)
+        state_84 = expand_to_84(state)
+        state_84 = state_84.float()
+        print(state_84)
+        output = self.forward(state_84)
         neural_distribution = output[:7]
         value = output[7]
 
-        tree_search = MCTS(model=self, iterations=100)
+        tree_search = MCTS(model=self, iterations=250)
         state = state.reshape(6, 7)
-        dist = tree_search.run(state=state, exploration_constant=exploration_constant, display=False)
+        dist = tree_search.run(state=state,
+                               exploration_constant=exploration_constant,
+                               epsilon=0,
+                               display=False)
+                               
         root_node = tree_search.explored_nodes[0]
         root_value = root_node.value_sum / root_node.visit_count
 
@@ -112,12 +145,17 @@ class Constant_Network:
         pass
 
     def forward(self, x):
-        normal = torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
-        state = x.reshape(6,7)
-        filter = mask(state)
-        normal_filtered = normal[filter]
-        normal_filtered = torch.softmax(normal_filtered, dim=0)
+        constant = torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+        state_me = x[:42].reshape(6,7) 
+        state_them = x[42:].reshape(6,7)
+
+        filter_me = mask(state_me)
+        filter_them = mask(state_them)
+        filter = (filter_me & filter_them)
+        constant_filtered = constant[filter]
+        constant_filtered = torch.softmax(constant_filtered, dim=0)
         distribution = torch.zeros(7)
-        distribution[filter] = normal_filtered
+        distribution[filter] = constant_filtered
         output = torch.cat((distribution, torch.tensor([0])))
         return output
+
